3cf2979b5fc3193eb7584c44d80d78497aa6732d
Boris Kaus <kaus@uni-mainz.de>
2023-01-19 21:05:52 +0100

    add meemum comparison fct

--- test/utils.jl
+++ test/utils.jl
@@ -197,3 +197,29 @@ function  read_chemical_potentials(file::String)
 
     return out
 end
+
+
+function compare_meemum_results(out::NamedTuple, out_expected::NamedTuple)
+
+    # Compare scalars
+    Compare_Fields = [:H, :S, :Cp, :variance, :spec_Cp, :P, :T]
+    @testset "test field $field" for field in Compare_Fields
+        @test out[field] ≈ out_expected[field]
+    end
+
+    # Compare fields
+    Compare_Fields = (:H2O, :MgO, :Al2O3, :K2O, :CaO, :TiO2, :FeO, :O2, :Na2O, :SiO2)
+    @testset "test chem. pot. $field" for field in Compare_Fields
+        @test out.chem_pot[field] ≈ out_expected.chem_pot[field] rtol=1e-4
+    end
+
+    # Compare phase composition
+    Compare_Fields = (Symbol("wt%"), Symbol("vol%"), Symbol("mol%"), :mol, :H2O, :MgO, :Al2O3, :K2O, :CaO, :TiO2, :FeO, :O2, :Na2O, :SiO2)
+    for i=1:length(out.phase_comp)
+     @testset "test $(out.phase_comp[i].Name), $field" for field in Compare_Fields
+            @test out.phase_comp[i][field] ≈ out_expected.phase_comp[i][field] rtol=2e-3
+        end
+    end
+
+    return nothing
+end
