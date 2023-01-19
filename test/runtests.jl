3bc3c4801ccd82432f4a2abe4bdabcc49dbd5b39
Boris Kaus <kaus@uni-mainz.de>
2023-01-19 15:36:45 +0100

    add way to read meemum output back into julia & use that in testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,5 +3,44 @@ using Test
 include("utils.jl")
 
 @testset "MEEMUM tests" begin
-    @test run_meemum(dir="bl691_MEEMUM", inputfile="bl691_MEEMUM_input.txt", dir_meemum="../sources/meemum") == true
+
+    dir_test = "bl691_MEEMUM";
+    dir_expected = "bl691_MEEMUM/output";
+    file_test = "bl691_MEEMUM"
+    file_input = "bl691_MEEMUM_input.txt"
+
+
+    out = run_test_pipe(dir=dir_test, inputfile=file_input, meemum=true)  
+    @test out == true
+
+    # Read expected & new files
+    out_expected = read_meemum(dir=dir_expected, filename=file_test);
+    out = read_meemum(dir=dir_test, filename=file_test);
+
+    # Compare scalars
+    Compare_Fields = [:H, :S, :Cp, :variance, :spec_Cp, :P, :T]
+    @testset "test field $field" for field in Compare_Fields
+        @test out[field] ≈ out_expected[field]
+    end
+
+    # Compare fields
+    Compare_Fields = (:H2O, :MgO, :Al2O3, :K2O, :CaO, :TiO2, :FeO, :O2, :Na2O, :SiO2)
+    @testset "test chem. pot. $field" for field in Compare_Fields
+        @test out.chem_pot[field] ≈ out_expected.chem_pot[field] rtol=1e-4
+    end
+
+    # Compare phase composition
+    Compare_Fields = (Symbol("wt%"), Symbol("vol%"), Symbol("mol%"), :mol, :H2O, :MgO, :Al2O3, :K2O, :CaO, :TiO2, :FeO, :O2, :Na2O, :SiO2)
+    for i=1:length(out.phase_comp)
+     @testset "test $(out.phase_comp[i].Name), $field" for field in Compare_Fields
+            @test out.phase_comp[i][field] ≈ out_expected.phase_comp[i][field] rtol=2e-3
+        end
+    end
+
 end
+
+@testset "Vertex klb691 test" begin
+    out = run_test_pipe(dir="klb691", inputfile="klb691_input.txt", vertex=true)  
+    @test out == true
+end
+
--- test/utils.jl
+++ test/utils.jl
@@ -1,27 +1,197 @@
 # This contains a few scripts to run Perple_X examples
 
+
 """
-success = run_meemum(; dir="bl691_MEEMUM", inputfile="bl691_MEEMUM_input.txt", dir_meemum="../sources/meemum")  
+    success = run_test_pipe(; dir="bl691_MEEMUM", inputfile="bl691_MEEMUM_input.txt", dir_exec="../sources/", meemum=false, vertex=false)  
 
-Runs a meemum test case in directory `dir`, using input file `inputfile`. The `meemum` executable is located in directory `dir_meemum` relative to the current directory.
+Runs a test case in directory `dir`, using input file `inputfile` that is piped into either the `vertex` or `meemum` executable. 
+The executable is located in directory `dir_exec` relative to the current directory.
 
-If something goes wrong, it will return false
+If something goes wrong, it will return false.
 """
-function run_meemum(; dir="bl691_MEEMUM", inputfile="bl691_MEEMUM_input.txt", dir_meemum="../sources/meemum")  
-cur_dir = pwd();
+function run_test_pipe(; dir="bl691_MEEMUM", inputfile="bl691_MEEMUM_input.txt", dir_exec="../sources/", meemum=false, vertex=false)  
+    cur_dir = pwd();
 
-# Try running; in case of error it'll return false
-success=false
-try 
-    cd(dir)
-    p = pipeline(`../$dir_meemum`, stdin=inputfile)
-    run(p)
-    success=true
-catch
+    # Indicate the executable you will run
+    exec=nothing
+    if meemum==true
+        exec = "meemum"
+    elseif vertex==true
+        exec = "vertex"
+    end
+    if isnothing(exec)
+        error("You need to indicate which executable you want to run")
+    end
+
+    # Try running the executable while piping the input file; in case of error it'll return false
     success=false
-end
-cd(cur_dir)
-return success
+    try 
+        dir_pipe = joinpath(cur_dir,dir)
+        cd(dir) # change to the directory
+        p = pipeline(`../$dir_exec/$exec`, stdin=inputfile)       # pipe input file to executable
+        @show p
+        run(p)  # run
+        success=true        
+    catch
+        success=false   # something errored
+    end
+    cd(cur_dir)
+    return success
 end
 
 
+
+
+"""
+
+Reads a meemum output file 
+"""
+function read_meemum(;filename="bl691_MEEMUM", dir="bl691_MEEMUM")
+
+    cur_dir = pwd();
+    file    = joinpath(cur_dir,dir,filename*".prn")
+
+    # Read floats from file
+    T,P, 
+    Cp, spec_Cp,
+    S,
+    H = extract_value(file, ("T(K)", "P(bar)",
+                                            "Heat Capacity (J/K/kg)","Specific Heat Capacity (J/K/m3)",
+                                            "Entropy (J/K/kg)",
+                                            "Enthalpy (J/kg)"
+                                            )) 
+
+    variance = extract_value(file, "Variance (c-p+2)")
+
+    seismic_props  = extract_data_block(file,"Seismic Properties:")
+    mol_props      = extract_data_block(file,"Molar Properties and Density:")
+    phase_comp     = extract_data_block(file,"Phase Compositions (molar  proportions):")
+    chem_pot       = read_chemical_potentials(file);
+    
+    cd(cur_dir)
+
+    # put all data in a Named Tuple
+    out = (T=T, P=P, Cp=Cp, spec_Cp=spec_Cp, S=S, H=H, variance=variance, 
+                chem_pot=chem_pot, seismic_props=seismic_props, phase_comp=phase_comp)
+
+    return out
+end
+
+
+"""
+    extract_value(file::String, keywords::NTuple{N,String}) 
+
+This scans the file `file` for `keywords` and returns the numerical extract_value 
+"""
+function  extract_value(file::String, keyword::NTuple{N,String}) where N
+    
+    d =  Vector{Union{Nothing, Float64}}(nothing, N)
+    open(file) do f
+        while ! eof(f) 
+            # read a new / next line for every iteration          
+            line = readline(f)
+            for i=1:N        
+                if contains(line, keyword[i])
+                    d[i] = parse(Float64,split(line,'=')[2]); 
+                end
+            end
+        end
+    end
+
+    return Tuple(d)
+end
+
+
+"""
+    extract_value(file::String, keyword::String)
+
+Extract a single value of a line
+"""
+function  extract_value(file::String, keyword::String)
+    return extract_value(file, (keyword,))[1]
+end
+
+
+"""
+
+    This extracts a data block from a figure
+"""
+function  extract_data_block(file::String, keyword::String, additional_empty_lines=0)
+    data = [];
+    open(file) do f
+        while ! eof(f) 
+            line = readline(f)
+            if contains(line, keyword)
+
+                line_names = readline(f)
+                line_names = replace(line_names, "Poisson ratio"=>"Poisson_ratio")
+
+                names = split(remove_space_beforeafter_symbol(line_names,"%"));
+                sym_names = Tuple([:Name; Symbol.(names)]);
+      
+                # read the block itself
+                while !isempty(line)
+                    line = readline(f)
+                    for i=1:additional_empty_lines
+                        line = readline(f)
+                    end
+                    line = replace(line, " - "=>"_")
+                    
+                    if !isempty(line)
+
+                        # convert values to float
+                        values = [split(line)[1]; parse.(Float64,split(line)[2:end])];
+                        
+
+                        # Store as NamedTuple
+                        data_line = NamedTuple{sym_names}(Tuple(values))
+
+                        push!(data, data_line)
+                    end
+                end
+          
+            end
+        end
+    end
+
+    return data
+end
+    
+function remove_space_beforeafter_symbol(str::String, sym::String)
+    
+    ind_all = findall(sym, str)
+    id  = [ind_all[i][1]-1 for i=1:length(ind_all)];
+    id1 = [ind_all[i][1]+1 for i=1:length(ind_all)];
+    
+    id_vec = setdiff(Vector(1:length(str)), id)
+    id_vec = setdiff(id_vec, id1)
+
+    str = str[id_vec]
+    return str
+end
+
+
+"""
+    out = read_chemical_potentials(file::String)
+reads chemical potentials from a meemum file
+"""
+function  read_chemical_potentials(file::String)
+    
+    out = nothing
+    open(file) do f
+        while ! eof(f) 
+            # read a new / next line for every iteration          
+            line = readline(f)    
+            if contains(line, "Chemical Potentials (J/mol):")
+                line = readline(f)  
+                names = Symbol.(split(readline(f)))
+                values = parse.(Float64,split(readline(f)))
+
+                out = NamedTuple{Tuple(names)}(Tuple(values))
+            end
+
+        end
+    end
+
+    return out
+end
